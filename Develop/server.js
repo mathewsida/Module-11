fc31291fbe07af92225e18f01c4dd18ba8b52de1
Mathew Sida-Nguon (H10 Capital) <sida.mathew@gmail.com>
2023-02-02 14:43:26 -0800

    fix route routing

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,8 @@ const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const path = require('path');
+const apiRoutes = require('./Routes/APIRoutes/APIRoutes');
+const htmlRoutes = require('./Routes/HTML Routes/HTMLRoutes');
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,3 +15,4 @@ app.use('/', htmlRoutes);
 app.listen(PORT, () => {
     console.log(`Now listening on PORT: ${PORT}`);
 });
+
